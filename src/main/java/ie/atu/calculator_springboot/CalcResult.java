9aafb8b8239dfc2cecdc2fd31c42cdf1cb53b3f9
James Albright <G00379271@atu.ie>
2023-09-27 20:24:29 +0100

    Refactored code to return an object and not one type such as an int. The returned object holds the operation type and the total result. It is formatted as operation: type, total, value. This is how it is viewed when as a JSON obj

--- src/main/java/ie/atu/calculator_springboot/CalcResult.java
+++ src/main/java/ie/atu/calculator_springboot/CalcResult.java
@@ -6,19 +6,45 @@ import lombok.Setter;
 @Getter
 @Setter
 
+
+//Created a class to store important variables that we wanted to return
 public class CalcResult {
 
-    private int num1;
-    private int num2;
+    private String operation;
+    private int total;
 
-    public int add() {
-        return num1+num2;
+
+    //holds the variables and allows us to return two different values in the same object
+    public CalcResult(String operation, int total) {
+
+        this.operation = operation;
+        this.total = total;
     }
 
-    public int subtract() { return num1-num2; }
+    CalcResult add(int num1, int num2) {
+        operation = "add";
+        total = num1 + num2;
+        //returns object holding value set in this method
+        return new CalcResult(operation, total);
+    }
 
-    public int multiply() { return num1*num2; }
+    CalcResult subtract(int num1, int num2) {
+        operation = "subtract";
+        total = num1 - num2;
+        return new CalcResult(operation, total);
+    }
 
-    public int divide() {return num1/num2;}
+    CalcResult multiply(int num1, int num2) {
+        operation = "multiply";
+        total = num1 * num2;
+        return new CalcResult(operation, total);
+    }
+
+    CalcResult divide(int num1, int num2) {
+        operation = "divide";
+        total = num1 / num2;
+        return new CalcResult(operation, total);
+    }
+
 
 }
--- src/main/java/ie/atu/calculator_springboot/CalculatorController.java
+++ src/main/java/ie/atu/calculator_springboot/CalculatorController.java
@@ -7,26 +7,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalculatorController {
 
+    //made the method of type object to return an instance of CalcResult class
     @GetMapping("/calculate")
-    public int calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation) {
+    public Object calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation) {
 
-        CalcResult calcResult = new CalcResult();
-        calcResult.setNum1(num1);
-        calcResult.setNum2(num2);
+        //create new instance of CalcResult
+        CalcResult calcResult = new CalcResult("", 0);
 
         switch (operation)  {
 
             case "add":
-                return calcResult.add();
+                return calcResult.add(num1, num2);
 
             case "subtract":
-                return calcResult.subtract();
+                return calcResult.subtract(num1, num2);
 
             case "multiply":
-                return calcResult.multiply();
+                return calcResult.multiply(num1, num2);
 
             case "divide":
-                return calcResult.divide();
+                return calcResult.divide(num1, num2);
 
         }
 
